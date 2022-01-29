7c8cca1ad6dcb907e5d2423711f94168fc74ca42
Marcos Felipe <mfelipesp@gmail.com>
2022-01-29 16:50:29 -0300

    Initial Commit

---
+++ PictureInPictureSample/PictureInPictureSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictureInPictureSampleApp.swift
+//  PictureInPictureSample
+//
+//  Created by marcos.felipe.souza on 29/01/22.
+//
+
+import SwiftUI
+
+@main
+struct PictureInPictureSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
